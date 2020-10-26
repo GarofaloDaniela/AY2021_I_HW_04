8c622dbc85b9d527d54b3a946eb70530aa154242
Daniela Garofalo <daniela.garofalo@mail.polimi.it>
2020-10-26 12:29:18 +0100

    Implementation of UART_CustomFunctions

--- GAROFALO_DANIELA.cydsn/UART_CustomFunctions.c
+++ GAROFALO_DANIELA.cydsn/UART_CustomFunctions.c
@@ -10,4 +10,55 @@
  * ========================================
 */
 
+/*
+    File UART_CustomFunctions.c
+    Functions aimed to ensure the proper operation of the UART component with respect to the requirements
+    Author: Daniela Garofalo
+*/
+    
+#include <UART_CustomFunctions.h>
+    
+int flag_received;
+int flag_start;
+int flag_stop;
+char received_data;
+
+CY_ISR(custom_UART_ISR)
+{
+    if (UART_ReadRxStatus() == UART_RX_STS_FIFO_NOTEMPTY) // Control that a data has been received
+    {
+        flag_received = 1; // Change the value of the flag according to the receiving of an information
+        LED_Blinking();
+    }
+}
+
+void UART_ReceivedData()
+{
+    if (flag_received == 1) // Received data
+    {
+        received_data = UART_GetByte(); // Function that gives the byte but also eventual error messages
+        switch (received_data) // Evaluation of the information received from the serial port
+        {
+            case 'B':
+            case 'b':
+            {
+                flag_start = 1; /* Starting the acquisition of potentiometer and photoresistor values
+                in order to switch ON the LED with the correct light intensity */
+                break;
+            }
+            case 'S':
+            case 's':
+            {
+                flag_stop = 1; /* Stopping the acquisition of the potentiometer and photoresistor values
+                and switching OFF the LED independently from its current state */
+                break;
+            }
+            default: // Operations done in all the other cases
+            {
+                break;
+            }
+        }
+    }
+}
+
 /* [] END OF FILE */
--- GAROFALO_DANIELA.cydsn/UART_CustomFunctions.h
+++ GAROFALO_DANIELA.cydsn/UART_CustomFunctions.h
@@ -10,4 +10,24 @@
  * ========================================
 */
 
+/*
+    File UART_CustomFunctions.h
+    Functions aimed to the inclusion and the definition of all the libraries and variables necessary for the source code
+    Author: Daniela Garofalo
+*/
+    
+#ifndef __UART_CUSTOMFUNCTIONS_H__
+    #define __UART_CUSTOMFUNCTIONS_H__
+    
+    #include <cytypes.h> // Library that contains the functions CY, included the one necessary to define the prototype of the ISR
+    #include <UART.h> // Library that contains all the functions associated to the UART component
+    #include <LED_pin.h> // Libraty that contains all the function associated to the LED component
+    
+#endif
+
+int flag_received; // Flag variable which is set equal to 1 when a data is received through the serial port
+
+CY_ISR_PROTO(custom_UART_ISR); // Declaration of the ISR function
+void UART_ReceivedData(); // Declaration of the function that controls the value of the received information
+
 /* [] END OF FILE */
